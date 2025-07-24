f58b084bfa7dea29bb91d2a73751ce4a2b987272
Alif-Sarker <sarker202454@gmil.com>
2025-07-24 18:08:17 +0600

    nabar added

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+
+const Homepage = () => {
+    return (
+        <div className='bg-[#21252a] min-h-screen'>
+            <Navbar></Navbar>
+            <h2>Banner</h2>
+        </div>
+    );
+};
+
+export default Homepage;
